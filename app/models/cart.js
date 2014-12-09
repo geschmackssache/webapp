2d0da7656c102d3a12d079dfbc44b58c11fd99f2
Gerrit Wanderer <wanderer@checkitmobile.com>
2014-12-09 18:13:10 +0100

    rebuild API for startscreen

--- app/models/cart.js
+++ app/models/cart.js
@@ -2,6 +2,13 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   user: DS.belongsTo('user'),
-  cart_items: DS.hasMany('cart-item'),
-  finished: DS.attr('boolean'),
+  cartItems: DS.hasMany('cart-item'),
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  street: DS.attr('string'),
+  zip: DS.attr('number'),
+  city: DS.attr('string'),
+  email: DS.attr('string'),
+  phone: DS.attr('string'),
+  finished: DS.attr('boolean')
 });
--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  cart: DS.belongsTo('cart')
+  cart: DS.belongsTo('cart'),
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  street: DS.attr('string'),
+  zip: DS.attr('number'),
+  city: DS.attr('string'),
+  phone: DS.attr('string'),
+  email: DS.attr('string')
 });
