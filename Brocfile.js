43c3c6a8a2ff2ad603a929711c4252929e17be11
Gerrit Wanderer <wanderer@checkitmobile.com>
2014-12-17 16:32:49 +0100

    setup twitter bootstrap

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,13 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+// Put the bootstrap fonts in the place that the bootstrap css expects to find them.
+var pickFiles = require('broccoli-static-compiler');
+var bootstrapFonts = pickFiles('bower_components/bootstrap/dist/fonts', {
+  srcDir: '/',
+  destDir: '/fonts'
+});
+// Merge the bootstrapFonts with the ember app tree
+var mergeTrees = require('broccoli-merge-trees');
+module.exports = mergeTrees([app.toTree(),bootstrapFonts]);
